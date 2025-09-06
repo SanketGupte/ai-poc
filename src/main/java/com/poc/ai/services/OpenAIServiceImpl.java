662de19cb88372baa7b2128758c5bb95be9fb401
Sanket Gupte <sanket.gupte542@gmail.com>
2025-09-06 20:46:16 +0530

    Vector Store Configuration

--- src/main/java/com/poc/ai/services/OpenAIServiceImpl.java
+++ src/main/java/com/poc/ai/services/OpenAIServiceImpl.java
@@ -1,13 +1,11 @@
 package com.poc.ai.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poc.ai.model.*;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.converter.BeanOutputConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
